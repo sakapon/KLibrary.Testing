5565d3dbf521647fc567ad51cbdc6fae32d56845
Keiho Sakapon <git@saka-pon.net>
2020-04-17 17:30:59 +0900

    Add helper class for assertion

---
+++ KLibrary4/Testing/Testing/AssertHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KLibrary.Testing
+{
+	public static class AssertHelper
+	{
+		public static Action<T, TResult> Create<T, TResult>(Func<T, TResult> target) =>
+			(arg, expected) => Assert.AreEqual(expected, target(arg));
+
+		public static Action<T1, T2, TResult> Create<T1, T2, TResult>(Func<T1, T2, TResult> target) =>
+			(arg1, arg2, expected) => Assert.AreEqual(expected, target(arg1, arg2));
+
+		public static Action<T1, T2, T3, TResult> Create<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> target) =>
+			(arg1, arg2, arg3, expected) => Assert.AreEqual(expected, target(arg1, arg2, arg3));
+	}
+}
