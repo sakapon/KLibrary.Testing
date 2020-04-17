9c4ded2dc72d7c38cbf8b1938ddff64613d6621b
Keiho Sakapon <git@saka-pon.net>
2020-04-17 17:39:39 +0900

    Add method

--- KLibrary4/Testing/Testing/AssertHelper.cs
+++ KLibrary4/Testing/Testing/AssertHelper.cs
@@ -13,5 +13,8 @@ namespace KLibrary.Testing
 
 		public static Action<T1, T2, T3, TResult> Create<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> target) =>
 			(arg1, arg2, arg3, expected) => Assert.AreEqual(expected, target(arg1, arg2, arg3));
+
+		public static Action<T, TResult[]> CreateForCollection<T, TResult>(Func<T, TResult[]> target) =>
+			(arg, expected) => CollectionAssert.AreEqual(expected, target(arg));
 	}
 }
