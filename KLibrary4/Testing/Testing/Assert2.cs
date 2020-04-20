52c37ba3c01bb7fa79c86d485a76aaff84b7cdf1
Keiho Sakapon <git@saka-pon.net>
2020-04-20 11:31:26 +0900

    Add extension class for assert

---
+++ KLibrary4/Testing/Testing/Assert2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KLibrary.Testing
+{
+	/// <summary>
+	/// Provides a set of methods to extend the <see cref="Assert"/> class.
+	/// </summary>
+	public static class Assert2
+	{
+		public static void AreNearlyEqual(float expected, float actual, int digits = 6)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void AreNearlyEqual(double expected, double actual, int digits = 12)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void AreNearlyEqual(decimal expected, decimal actual, int digits = 12)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void IsOfType<T>(object obj)
+		{
+			if (!(obj is T)) throw new AssertFailedException($"The object is not {typeof(T)}.");
+		}
+
+		public static void IsNotOfType<T>(object obj)
+		{
+			if (obj is T) throw new AssertFailedException($"The object is {typeof(T)}.");
+		}
+
+		public static T Throws<T>(Action action) where T : Exception
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
