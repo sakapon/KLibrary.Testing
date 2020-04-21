04ca4eab8482b2ae6a1f7ce880d9654da86419ac
Keiho Sakapon <git@saka-pon.net>
2020-04-21 19:28:23 +0900

    Add methods

--- KLibrary4/Testing/Testing/AssertHelper.cs
+++ KLibrary4/Testing/Testing/AssertHelper.cs
@@ -17,6 +17,18 @@ namespace KLibrary.Testing
 		public static Action<T1, T2, T3, TResult> Create<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> target) =>
 			(arg1, arg2, arg3, expected) => Assert.AreEqual(expected, target(arg1, arg2, arg3));
 
+		public static Action<T, TResult> CreateForNearlyEqual<T, TResult>(Func<T, TResult> target)
+		{
+			if (typeof(TResult) == typeof(float))
+				return (arg, expected) => Assert2.AreNearlyEqual((float)(object)expected, (float)(object)target(arg));
+			else if (typeof(TResult) == typeof(double))
+				return (arg, expected) => Assert2.AreNearlyEqual((double)(object)expected, (double)(object)target(arg));
+			else if (typeof(TResult) == typeof(decimal))
+				return (arg, expected) => Assert2.AreNearlyEqual((decimal)(object)expected, (decimal)(object)target(arg));
+			else
+				throw new InvalidOperationException("<TResult> is invalid.");
+		}
+
 		public static Action<T, TResult[]> CreateForCollection<T, TResult>(Func<T, TResult[]> target) =>
 			(arg, expected) => CollectionAssert.AreEqual(expected, target(arg));
 	}
