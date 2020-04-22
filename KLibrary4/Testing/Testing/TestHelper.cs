29aee8bef1650eff70e7e61e66266699618dba9e
Keiho Sakapon <git@saka-pon.net>
2020-04-22 12:24:50 +0900

    Check null

--- KLibrary4/Testing/Testing/TestHelper.cs
+++ KLibrary4/Testing/Testing/TestHelper.cs
@@ -8,14 +8,23 @@ namespace KLibrary.Testing
 	/// </summary>
 	public static class TestHelper
 	{
-		public static Action<T, TResult> Create<T, TResult>(Func<T, TResult> target) =>
-			(arg, expected) => Assert.AreEqual(expected, target(arg));
+		public static Action<T, TResult> CreateAreEqual<T, TResult>(Func<T, TResult> target)
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+			return (arg, expected) => Assert.AreEqual(expected, target(arg));
+		}
 
-		public static Action<T1, T2, TResult> Create<T1, T2, TResult>(Func<T1, T2, TResult> target) =>
-			(arg1, arg2, expected) => Assert.AreEqual(expected, target(arg1, arg2));
+		public static Action<T1, T2, TResult> CreateAreEqual<T1, T2, TResult>(Func<T1, T2, TResult> target)
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+			return (arg1, arg2, expected) => Assert.AreEqual(expected, target(arg1, arg2));
+		}
 
-		public static Action<T1, T2, T3, TResult> Create<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> target) =>
-			(arg1, arg2, arg3, expected) => Assert.AreEqual(expected, target(arg1, arg2, arg3));
+		public static Action<T1, T2, T3, TResult> CreateAreEqual<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> target)
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+			return (arg1, arg2, arg3, expected) => Assert.AreEqual(expected, target(arg1, arg2, arg3));
+		}
 
 		public static Action<object[], object> CreateAreEqual(Delegate target)
 		{
--- KLibrary4/UnitTest/Testing/TestHelperTest.cs
+++ KLibrary4/UnitTest/Testing/TestHelperTest.cs
@@ -10,6 +10,18 @@ namespace UnitTest.Testing
 	public class TestHelperTest
 	{
 		[TestMethod]
+		public void CreateAreEqual_Func_2()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => TestHelper.CreateAreEqual<double, int, double>(null));
+
+			var test = TestHelper.CreateAreEqual<double, int, double>(Math.Round);
+			Assert.ThrowsException<AssertFailedException>(() => test(1.23, 1, 0));
+
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => test(1.23, -1, 0));
+			test(1.23, 1, 1.2);
+		}
+
+		[TestMethod]
 		public void CreateAreEqual_Delegate_00()
 		{
 			var test = TestHelper.CreateAreEqual((Action)(() => Console.WriteLine("abc")));
@@ -28,9 +40,14 @@ namespace UnitTest.Testing
 		[TestMethod]
 		public void CreateAreEqual_Delegate_14()
 		{
-			int IndexOf(string s, char value, int startIndex, int count) => s.IndexOf(value, startIndex, count);
+			int IndexOf(string s, char value, int startIndex, int count) => s?.IndexOf(value, startIndex, count) ?? throw new ArgumentNullException(nameof(s));
+
+			Assert.ThrowsException<ArgumentNullException>(() => TestHelper.CreateAreEqual(null));
 
 			var test = TestHelper.CreateAreEqual((Func<string, char, int, int, int>)IndexOf);
+			Assert.ThrowsException<AssertFailedException>(() => test(new object[] { "abcde", 'c', 3, 2 }, 2));
+
+			Assert.ThrowsException<ArgumentNullException>(() => test(new object[] { null, 'c', 0, 0 }, null));
 			test(new object[] { "abcde", 'c', 0, 5 }, 2);
 			test(new object[] { "abcde", 'c', 3, 2 }, -1);
 		}
