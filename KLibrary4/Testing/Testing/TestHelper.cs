3907a18ed408478634a233f7d345c4b7c7014012
Keiho Sakapon <git@saka-pon.net>
2020-04-25 16:37:03 +0900

    Add comments

--- KLibrary4/Testing/Testing/TestHelper.cs
+++ KLibrary4/Testing/Testing/TestHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KLibrary.Testing
 {
@@ -9,6 +8,14 @@ namespace KLibrary.Testing
 	/// </summary>
 	public static class TestHelper
 	{
+		/// <summary>
+		/// Creates a test method from the target function, that asserts the return value is equal to the expected value.
+		/// Call the test method with arguments, those are the arguments of the target function and the expected value.
+		/// </summary>
+		/// <param name="func">The function to be tested. This must be a Func.</param>
+		/// <returns>A test method.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException">The delegate does not return a value.</exception>
 		public static Action<object[], object> CreateAreEqual(Delegate func)
 		{
 			if (func == null) throw new ArgumentNullException(nameof(func));
@@ -16,6 +23,15 @@ namespace KLibrary.Testing
 			return (args, expected) => Assert2.AreEqual(expected, func.Invoke(args));
 		}
 
+		/// <summary>
+		/// Creates a test method from the target function, that asserts the return value is equal to the expected value.
+		/// Call the test method with arguments, those are the arguments of the target function and the expected value.
+		/// </summary>
+		/// <typeparam name="T">The type of the parameter of the target function.</typeparam>
+		/// <typeparam name="TResult">The type of the return value of the target function.</typeparam>
+		/// <param name="func">The function to be tested.</param>
+		/// <returns>A test method.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
 		public static Action<T, TResult> CreateAreEqual<T, TResult>(Func<T, TResult> func)
 		{
 			if (func == null) throw new ArgumentNullException(nameof(func));
@@ -34,6 +50,15 @@ namespace KLibrary.Testing
 			return (arg1, arg2, arg3, expected) => Assert2.AreEqual(expected, func(arg1, arg2, arg3));
 		}
 
+		/// <summary>
+		/// Creates a test method from the target function, that asserts the return value is nearly equal to the expected value.
+		/// Call the test method with arguments, those are the arguments of the target function and the expected value.
+		/// </summary>
+		/// <param name="func">The function to be tested. This must be a Func.</param>
+		/// <param name="digits">The digits that represent the upper bound of the absolute error between the two values.</param>
+		/// <returns>A test method.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException">The delegate does not return a value.</exception>
 		public static Action<object[], object> CreateAreNearlyEqual(Delegate func, int digits = -12)
 		{
 			if (func == null) throw new ArgumentNullException(nameof(func));
@@ -41,6 +66,16 @@ namespace KLibrary.Testing
 			return (args, expected) => Assert2.AreNearlyEqual(expected, func.Invoke(args), digits);
 		}
 
+		/// <summary>
+		/// Creates a test method from the target function, that asserts the return value is nearly equal to the expected value.
+		/// Call the test method with arguments, those are the arguments of the target function and the expected value.
+		/// </summary>
+		/// <typeparam name="T">The type of the parameter of the target function.</typeparam>
+		/// <typeparam name="TResult">The type of the return value of the target function.</typeparam>
+		/// <param name="func">The function to be tested.</param>
+		/// <param name="digits">The digits that represent the upper bound of the absolute error between the two values.</param>
+		/// <returns>A test method.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
 		public static Action<T, TResult> CreateAreNearlyEqual<T, TResult>(Func<T, TResult> func, int digits = -12)
 		{
 			if (func == null) throw new ArgumentNullException(nameof(func));
@@ -59,7 +94,7 @@ namespace KLibrary.Testing
 			return (arg1, arg2, arg3, expected) => Assert2.AreNearlyEqual(expected, func(arg1, arg2, arg3), digits);
 		}
 
-		/// <exception cref="ArgumentException">The method represented by the delegate is invoked on an object or a class that does not support it.</exception>
+		/// <exception cref="ArgumentException">The elements of the parameters array do not match the signature of the delegate.</exception>
 		/// <exception cref="Exception">Any other exception on target invocation.</exception>
 		static object Invoke(this Delegate target, object[] args)
 		{
