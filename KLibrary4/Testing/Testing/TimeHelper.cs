15d0ce9180f02623b67e1760f3f8112f66926ea7
Keiho Sakapon <git@saka-pon.net>
2020-04-27 18:32:59 +0900

    Add comments

--- KLibrary4/Testing/Testing/TimeHelper.cs
+++ KLibrary4/Testing/Testing/TimeHelper.cs
@@ -4,10 +4,16 @@ using System.Diagnostics;
 namespace KLibrary.Testing
 {
 	/// <summary>
-	/// Provides a set of methods to measure time for execution.
+	/// Provides a set of methods to measure execution time.
 	/// </summary>
 	public static class TimeHelper
 	{
+		/// <summary>
+		/// Invokes the action and writes the execution time to the standard output.
+		/// </summary>
+		/// <param name="action">An action.</param>
+		/// <param name="category">A category name. This is optional.</param>
+		/// <exception cref="ArgumentNullException"></exception>
 		public static void Measure(Action action, string category = null)
 		{
 			if (action == null) throw new ArgumentNullException(nameof(action));
@@ -18,7 +24,15 @@ namespace KLibrary.Testing
 			Console.WriteLine(string.IsNullOrEmpty(category) ? $"{sw.Elapsed}" : $"{category}: {sw.Elapsed}");
 		}
 
-		public static T Measure<T>(Func<T> func, string category = null)
+		/// <summary>
+		/// Invokes the function and writes the execution time to the standard output.
+		/// </summary>
+		/// <typeparam name="TResult">The type of the return value of the function.</typeparam>
+		/// <param name="func">A function.</param>
+		/// <param name="category">A category name. This is optional.</param>
+		/// <returns>The return value of the function.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		public static TResult Measure<TResult>(Func<TResult> func, string category = null)
 		{
 			if (func == null) throw new ArgumentNullException(nameof(func));
 
